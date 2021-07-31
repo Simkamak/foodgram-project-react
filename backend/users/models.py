45a267a9bed6330ed5353ee60367c059bc4dfd3e
Таисия Горностаева <taisiagornostaeva@MacBook-Pro-Taisia.local>
2021-07-31 14:02:23 +0300

    Create CustomUser

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        unique=True,
+        max_length=150
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=1
+    )
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        unique=True,
+        max_length=254
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+
